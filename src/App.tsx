6c87f650929e48d4a57b1706e17cad46ed43fbbc
Noëllie Chatain Six <noellie.chatain-six@oclock.school>
2024-06-03 08:39:23 +0200

    import outlet

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { Outlet } from 'react-router';
+
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 import ScreenSize from './components/devComponent/ScreenSize';
@@ -6,7 +8,7 @@ function App() {
   return (
     <div className="app-container h-screen w-screen flex flex-col bg-custom-gradient">
       <Header />
-      <p>My Body</p>
+      <Outlet />
       <Footer />
       <ScreenSize />
     </div>
