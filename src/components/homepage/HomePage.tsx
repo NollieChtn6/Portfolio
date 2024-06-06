2711b54a8fbae2bf00134547d69442a5191e86b7
Noëllie Chatain Six <noellie.chatain-six@oclock.school>
2024-06-06 12:22:31 +0200

    remove grid layout for better responsive display

--- src/components/homepage/HomePage.tsx
+++ src/components/homepage/HomePage.tsx
@@ -1,24 +1,35 @@
+import { NavLink } from 'react-router-dom';
+
 import { Button } from '../ui/button';
 import ScreenComponent from './screen/Screen';
 
 function HomePage() {
   return (
-    <main className="grid grid-cols-1 md:grid-cols-2 grid-rows-2 md:grid-rows-1 gap-4 p-6 text-woodsmoke-200 h-full">
-      <div className="flex flex-col homepage-left-side space-y-6 p-12 place-content-center">
-        <h2 className="hero-title flex w-full text-4xl">Noëllie Chatain Six</h2>
-        <p className="hero-subtitle flex w-4/5 text-xl animate-typing overflow-hidden whitespace-nowrap font-mono border-r-4">
-          Développeuse web junior
-        </p>
-        <p>
-          J&rsquo;ai troqué les dictionnaires pour les lignes de code. Bienvenue
-          sur mon portfolio&nbsp;!
-        </p>
-        <div className="homepage-btn-container flex w-3/4 self-center justify-around">
-          <Button>À propos</Button>
-          <Button>Mes projets</Button>
+    <main className="homepage-container flex flex-col md:flex-row max-sm:overflow-y-scroll h-full">
+      <div className="homepage-content-left flex flex-col md:flex-row w-full md:w-2/4">
+        <div className="about-text-container text-woodsmoke-100 flex flex-col grow md:h-full  p-9 md:w-2/4 space-y-6 md:place-content-center">
+          <h1 className="text-woodsmoke-100 text-3xl md:text-4xl uppercase">
+            Noëllie Chatain Six
+          </h1>
+          <p className="hero-subtitle flex w-4/5 text-lg md:text-xl animate-typing overflow-hidden whitespace-nowrap font-mono border-r-4">
+            Développeuse web junior
+          </p>
+          <p>
+            J&rsquo;ai troqué les dictionnaires linguistiques pour les lignes de
+            code.
+            <br /> Bienvenue sur mon portfolio&nbsp;!
+          </p>
+          <div className="homepage-btns-container flex justify-evenly w-full md:w-3/4 self-center">
+            <NavLink to="/about">
+              <Button>À propos</Button>
+            </NavLink>
+            <NavLink to="/projects">
+              <Button>Projets</Button>
+            </NavLink>
+          </div>
         </div>
       </div>
-      <div className="homepage-right-side flex space-y-6 md:p-12 justify-center items-center overflow-y-auto">
+      <div className="homepage-content-right flex h-full p-9 md:w-2/4 place-content-center items-center">
         <ScreenComponent />
       </div>
     </main>
