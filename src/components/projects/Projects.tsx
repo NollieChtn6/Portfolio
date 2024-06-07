281a72192dd6b45b0eb1c0f76397deb6d83b0382
Noëllie Chatain Six <noellie.chatain-six@oclock.school>
2024-06-07 20:52:05 +0200

    update projects page layout

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,19 +1,49 @@
+import { useState } from 'react';
+
+import ProjectElement from './ProjectElement';
+import { Switch } from '../ui/switch';
+import { Label } from '../ui/label';
+
+import projects from '../../data/projects.json';
+
 function Projects() {
+  const projectsList = projects;
+  const sandboxProjects = projectsList.filter(
+    (sandboxProject) => sandboxProject.sandbox === true
+  );
+  const [displaySandboxOnly, setDisplaySandboxOnly] = useState(false);
+
   return (
-    <main className="projects-container flex flex-col md:flex-row max-sm:overflow-y-scroll h-full">
-      <div className="projects-title flex w-full h-[10%] md:h-full md:w-[10%] items-center md:justify-center">
+    <main className="projects-container flex flex-col md:flex-row h-full overflow-hidden">
+      <div className="projects-title flex w-full md:h-full md:w-[10%] items-center md:justify-center">
         <h2 className="text-woodsmoke-200 md:rotate-180 md:[writing-mode:vertical-rl] text-3xl p-6 uppercase tracking-wide font-mono">
           Projets
         </h2>
       </div>
-      <div className="projects-content flex flex-col md:flex-row w-full h-[90%] md:h-full">
-        <div className="projects-content-container text-woodsmoke-200 flex flex-col grow md:h-full p-9 md:p-6 md:w-2/4 space-y-6 md:place-content-center">
-          <p className="text-content text-md font-light">
-            Oups&nbsp;! Cette page est en cours de construction&nbsp;! Elle sera
-            bientôt accessible.
-          </p>
+      <div className="projects-content flex flex-col w-full h-full p-3 items-center space-y-6 overflow-hidden">
+        <div className="w-full h-[100px] px-3 place">
+          <div className="flex items-center space-x-2 h-full justify-end">
+            <Label
+              htmlFor="display-sandbox-projects"
+              className="font-mono text-woodsmoke-200"
+            >
+              Sandbox
+            </Label>
+            <Switch
+              id="display-sandbox-projects"
+              onCheckedChange={() => setDisplaySandboxOnly(!displaySandboxOnly)}
+            />
+          </div>
         </div>
-        <div className="projects-img-container flex h-2/4 md:h-full p-9 md:w-2/4 max-sm:hidden place-content-center items-center" />
+        <div className="projects-list flex flex-col grow md:flex-row md:flex-wrap p-3 justify-evenly md:justify-evenly overflow-y-auto w-full">
+          {displaySandboxOnly
+            ? sandboxProjects.map((project) => {
+                return <ProjectElement key={project.id} project={project} />;
+              })
+            : projectsList.map((project) => {
+                return <ProjectElement key={project.id} project={project} />;
+              })}
+        </div>
       </div>
     </main>
   );
