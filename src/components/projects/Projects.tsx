f0f432dbd90cb706b403158c8710359e13aa4d9f
Noëllie Chatain Six <noellie.chatain@hotmail.com>
2024-07-05 14:28:51 +0200

    fix font color on switch label

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -16,16 +16,14 @@ function Projects() {
   return (
     <main className="projects-container flex flex-col md:flex-row h-full overflow-hidden">
       <div className="projects-title flex w-full md:h-full md:w-[10%] items-center md:justify-center">
-        <h2 className="text-woodsmoke-200 md:rotate-180 md:[writing-mode:vertical-rl] text-3xl p-6 uppercase tracking-wide font-mono">
-          Projets
-        </h2>
+        <h2 className="page-title">Projets</h2>
       </div>
       <div className="projects-content flex flex-col w-full h-full p-3 items-center space-y-6 overflow-hidden">
         <div className="w-full h-[50px] px-3 max-md:hidden md:display">
           <div className="flex items-center space-x-2 h-full justify-end">
             <Label
               htmlFor="display-sandbox-projects"
-              className="font-mono text-woodsmoke-200"
+              className="font-mono text-mine-shaft-100"
             >
               Sandbox
             </Label>
