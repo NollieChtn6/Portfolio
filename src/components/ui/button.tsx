6725833e97c3176064ab8b57addeb60e3d626084
Noëllie Chatain Six <noellie.chatain@hotmail.com>
2024-07-05 12:26:11 +0200

    update ui components style and layout based on brand new color palette

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -11,22 +11,25 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          'bg-royal-blue-900 text-slate-50 hover:bg-royal-blue-700/90 dark:bg-slate-50 dark:text-slate-900 dark:hover:bg-slate-50/90 uppercase text-xs font-mono',
+          'bg-grandis-300 text-mine-shaft-700 hover:bg-grandis-200 hover:scale-105 transition delay-50 uppercase text-sm font-mono',
         destructive:
-          'bg-red-500 text-slate-50 hover:bg-red-500/90 dark:bg-red-900 dark:text-slate-50 dark:hover:bg-red-900/90',
+          'bg-red-500 text-slate-50 hover:bg-red-500/90 hover:scale-105 dark:bg-red-900 dark:text-slate-50 dark:hover:bg-red-900/90',
         outline:
-          'border border-royal-blue-500 hover:bg-royal-blue-400 hover:text-slate-900 dark:border-slate-800 dark:bg-slate-950 dark:hover:bg-slate-800 dark:hover:text-slate-50',
+          'border text-wild-sand-100 border-grandis-300 hover:bg-grandis-200 hover:text-mine-shaft-700 hover:scale-105 transition delay-50 uppercase text-xs font-mono',
         secondary:
           'bg-slate-100 text-slate-900 hover:bg-slate-100/80 dark:bg-slate-800 dark:text-slate-50 dark:hover:bg-slate-800/80',
         ghost:
-          'hover:bg-slate-100 hover:text-slate-900 dark:hover:bg-slate-800 dark:hover:text-slate-50 text-lg uppercase text-royal-blue-400 ',
+          'hover:bg-mine-shaft-300 hover:text-slate-900 dark:hover:bg-slate-800 dark:hover:text-slate-50 text-lg uppercase text-royal-blue-400 ',
         link: 'text-slate-900 underline-offset-4 hover:underline dark:text-slate-50',
+        mobile:
+          'bg-grandis-300 text-mine-shaft-700 hover:bg-grandis-200 uppercase text-lg font-mono',
       },
       size: {
-        default: 'h-10 px-4 py-2',
+        default: 'h-10 px-4 py-2 w-[100px]',
         sm: 'h-9 rounded-md px-3',
         lg: 'h-11 rounded-md px-8',
         icon: 'h-10 w-10',
+        cta: 'h-11 rounded-md px-8 w-[180px]',
       },
     },
     defaultVariants: {
--- src/components/ui/switch.tsx
+++ src/components/ui/switch.tsx
@@ -9,7 +9,7 @@ const Switch = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <SwitchPrimitives.Root
     className={cn(
-      'peer inline-flex h-6 w-11 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 focus-visible:ring-offset-white disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-royal-blue-600 data-[state=unchecked]:bg-royal-blue-950 dark:focus-visible:ring-slate-300 dark:focus-visible:ring-offset-slate-950 dark:data-[state=checked]:bg-slate-50 dark:data-[state=unchecked]:bg-slate-800',
+      'peer inline-flex h-6 w-11 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 focus-visible:ring-offset-white disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-grandis-400 data-[state=unchecked]:bg-mine-shaft-600',
       className
     )}
     {...props}
@@ -17,7 +17,7 @@ const Switch = React.forwardRef<
   >
     <SwitchPrimitives.Thumb
       className={cn(
-        'pointer-events-none block h-5 w-5 rounded-full bg-royal-blue-200 shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0 dark:bg-slate-950'
+        'pointer-events-none block h-5 w-5 rounded-full bg-mine-shaft-200 shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0 dark:bg-slate-950'
       )}
     />
   </SwitchPrimitives.Root>
