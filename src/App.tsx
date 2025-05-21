c0594c12d485b61c3b1310351704778e500b2009
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2025-05-21 21:57:16 +0200

    fetch data on app loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
-import { Outlet } from 'react-router';
+import { useEffect } from "react";
+import { Outlet } from "react-router";
 
-import Header from './components/header/Header';
-import Footer from './components/footer/Footer';
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
 // import ScreenSize from './components/devComponent/ScreenSize';
 
-function App() {
+import { useProjectsStore } from "./store/projectsStore";
+import { useSkillsStore } from "./store/skillsStore";
+import { useMilestonesStore } from "./store/milestonesStore";
+
+export function App() {
+  const { fetchProjects } = useProjectsStore();
+  const { fetchSkills } = useSkillsStore();
+  const { fetchMilestones } = useMilestonesStore();
+
+  useEffect(() => {
+    fetchMilestones();
+    fetchProjects();
+    fetchSkills();
+  }, [fetchMilestones, fetchProjects, fetchSkills]);
   return (
-    <body className="body h-screen w-screen flex flex-col bg-[url('/background/circuit-primary.svg')] p-3">
-      <div className="app-container flex flex-col h-full w-full xl:max-w-[1280px] xl:self-center">
+    <body className="body h-screen w-screen flex flex-col bg-[url('/background/hexagon.svg')]">
+      <div className="app-container flex flex-col h-full w-full xl:self-center">
         <Header />
         <Outlet />
         <Footer />
@@ -16,5 +30,3 @@ function App() {
     </body>
   );
 }
-
-export default App;
