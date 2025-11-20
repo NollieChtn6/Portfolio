e7f20220d8026c3d92ea157fe1f2ed5e9b2ffe35
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2025-11-20 21:37:52 +0100

    remove Hearder and Footer from their own folder and update styles and layout

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="footer-container">
+      <p className="copyright-content font-mono text-sm text-iron-50">
+        © NC•6 | Tous droits réservés
+      </p>
+    </footer>
+  );
+}
--- src/components/footer/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="footer-container flex h-16 mt-auto items-center p-6">
-      <p className="copyright-content font-mono text-sm text-firefly-400">
-        © NC•6 | Tous droits réservés
-      </p>
-    </footer>
-  );
-}
